b3c534a429fb618ab172f74d1c79fa6b6a0da0b0
Sergey Shapirenko <s.shapirenko@hsdigital.ru>
2019-04-15 15:18:52 +0700

    added payload type

--- src/store/mock/actions.ts
+++ src/store/mock/actions.ts
@@ -3,7 +3,7 @@ import { ActionTree } from 'vuex';
 import { IRootState, IMockState } from '@/types';
 
 export const actions: ActionTree<IMockState, IRootState> = {
-    mockAction({ commit }, payload) {
+    mockAction({ commit }, payload: string) {
         commit('mockAction', payload);
     }
 };
