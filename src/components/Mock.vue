51f8bf640b4b592f4cf97d2fb377e2c5be658d5b
Sergey Shapirenko <sshapirenko@gmail.com>
2019-08-09 14:52:53 +0700

    add format script, example with props

--- src/components/Mock.vue
+++ src/components/Mock.vue
@@ -1,15 +1,25 @@
 <template lang="pug">
 .mock
     | {{ mockValue }}
+    Mock2.mock2(
+        text="foo"
+    )
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { Getter, namespace } from 'vuex-class';
 
+// поправить расширение файлов при импорте
+import Mock2 from './Mock2.vue';
+
 const mock = namespace('mock');
 
-@Component
+@Component({
+    components: {
+        Mock2
+    }
+})
 export default class Mock extends Vue {
     @mock.Action('mockAction') mockAction!: (payload: string) => void;
 
---
+++ src/components/Mock2.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+.mock2
+    | hello from text prop {{ text }}
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class Mock2 extends Vue {
+    @Prop({
+        type: String,
+        default: '',
+        required: true
+    })
+    text!: string;
+}
+</script>
